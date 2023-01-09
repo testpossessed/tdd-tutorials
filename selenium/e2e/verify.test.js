70a25829655a971cad4b4781d32fd2e159431564
Mike Hanson <mike@testpossessed.com>
2023-01-09 07:44:35 +0000

    Added verification test

---
+++ selenium/e2e/verify.test.js
@@ -0,0 +1,22 @@
+const {By, Builder} = require('selenium-webdriver');
+const {suite} = require('selenium-webdriver/testing');
+const {assert} = require('assert');
+
+suite(function(env) {
+  describe('Verify Selenium Setup', function() {
+    let driver;
+  
+    before(async function() {
+      driver = await new Builder().forBrowser('chrome').build();
+    });
+    
+    after(async () => await driver.quit());
+    
+    it('Should have the correct title', async function() {
+      await driver.get('http://localhost:3000');
+      
+      let title = await driver.getTitle();
+      assert.equal("Express", title);      
+    });
+  });
+});
